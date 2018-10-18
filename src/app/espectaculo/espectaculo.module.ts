824ff1f9055262f2fe807aeec237da4aded52fb0
srodriguezb1 <s.rodriguezb1@uniandes.edu.co>
2018-10-18 08:53:32 -0500

    Espectaculos y merge

---
+++ src/app/espectaculo/espectaculo.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EspectaculoListComponent } from './espectaculo-list/espectaculo-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EspectaculoListComponent]
+})
+export class EspectaculoModule { }
