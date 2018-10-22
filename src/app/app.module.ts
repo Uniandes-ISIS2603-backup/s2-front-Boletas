281713ced4f7d8e7478228386b53454a30006817
gaHamilton <g.hamilton@uniandes.edu.co>
2018-10-22 12:20:40 -0500

    quite el import de toastr en app module para evitar problema al corre por ahora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; 
 import { HttpErrorInterceptor } from './interceptors/httperrorinterceptor.service';
-import { ToastrModule } from 'ngx-toastr';
+
 
 
 import { AppComponent } from './app.component';
