dd469081b5f27342be4c899de3d78357dc2cda8e
Jhonatan Amórtegui <ja.amortegui10@uniandes.edu.co>
2018-10-23 08:38:48 -0500

    Componentes detail de Silla y Lugar agregados.

---
+++ src/app/lugar/lugar-detail/lugar-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lugar-detail',
+  templateUrl: './lugar-detail.component.html',
+  styleUrls: ['./lugar-detail.component.css']
+})
+export class LugarDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
