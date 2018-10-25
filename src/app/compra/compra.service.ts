7df670b9dcc019739e12a7dc81c0643ff8fdb31c
gaHamilton <g.hamilton@uniandes.edu.co>
2018-10-25 10:14:09 -0500

    cambios en compra list(para detail), module(routing), service(datos json para probar detail, temporal), app component (poner la pestania de compras)

--- src/app/compra/compra.service.ts
+++ src/app/compra/compra.service.ts
@@ -6,15 +6,16 @@ import { environment } from '../../environments/environment';
 
 import { CompraDetail } from './compra-detail';
 
-const API_URL = environment.apiURL;
-const compras = '/compras';
-
+//const API_URL = environment.apiURL;
+//const compras = '/compras';
+const API_URL = '../../assets/';
+const compras = 'compras.json';
 @Injectable()
 export class CompraService {
     
     constructor(private http: HttpClient) { }
     
-    getCompraDetail(compraId): Observable<CompraDetail> {
+    getCompraDetail(compraId: number): Observable<CompraDetail> {
         return this.http.get<CompraDetail>(API_URL + compras + '/' + compraId);
     }
     
