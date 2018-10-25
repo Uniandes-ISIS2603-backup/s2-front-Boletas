9d8c999996b1ea20f972f243c7c5b654a1835fff
gaHamilton <g.hamilton@uniandes.edu.co>
2018-10-25 12:23:00 -0500

    modificaciones en compra para el paso 3

--- src/app/compra/compra-list/compra-list.component.ts
+++ src/app/compra/compra-list/compra-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Compra } from '../compra';
 import { CompraService } from '../compra.service';
+import {CompraDetail} from '../compra-detail';
 
 
 @Component({
@@ -12,6 +13,23 @@ import { CompraService } from '../compra.service';
 export class CompraListComponent implements OnInit {
     constructor(private compraService: CompraService) { } 
     compras: Compra[];
+    compra_id: number;
+    selectedCompra : Compra;
+    
+    onSelected(compra_id: number):void {
+        this.compra_id = compra_id;
+        this.selectedCompra = new CompraDetail();
+        this.getCompraDetail();        
+    }
+    
+    getCompraDetail(): void {
+        this.compraService.getCompraDetail(this.compra_id)
+            .subscribe(selectedCompra => {
+                this.selectedCompra = selectedCompra
+            });
+    }
+    
+    
     getCompras(): void {
         this.compraService.getCompras().subscribe(compras => this.compras = compras);
     }
