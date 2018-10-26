234682726d9ad2771dfe578d6487d827cf62647a
gaHamilton <g.hamilton@uniandes.edu.co>
2018-10-25 20:47:30 -0500

    compra create (incompleto)

--- src/app/compra/compra-create/compra-create.component.ts
+++ src/app/compra/compra-create/compra-create.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {CompraService} from '../compra.service';
+import {ToastrService} from 'ngx-toastr';
+import {Compra} from '../compra';
 
 @Component({
   selector: 'app-compra-create',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compra-create.component.css']
 })
 export class CompraCreateComponent implements OnInit {
+ 
+ constructor(
+        private compraService: CompraService,
+        private toastrService: ToastrService
+    ) { }
 
-  constructor() { }
+    compra: Compra;
 
-  ngOnInit() {
-  }
+    @Output() cancel = new EventEmitter();
+
+    @Output() create = new EventEmitter();
+
+    createCompra(): void {
+        var compra_create = {
+            envio: this.compra.envio,
+            direccion: this.compra.direccion
+        };
+        this.compraService.createCompra(compra_create)
+            .subscribe(() => {
+                this.create.emit();
+                this.toastrService.success("La compra fue creada", "Compra creation");
+            });
+    }
+    
+    cancelCreation(): void {
+        this.cancel.emit();
+    }
+    
+    ngOnInit() {
+        this.compra = new Compra();
+    }
 
 }
