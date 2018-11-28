ee82afd2ed54408cd01ed38945c7d646e95dd85a
jdcamachod <jd.camachod@uniandes.edu.co>
2018-11-28 17:57:30 -0500

    funcionando compra delete falta edit y cambios en el interceptor

--- src/app/compra/compra-edit/compra-edit.component.ts
+++ src/app/compra/compra-edit/compra-edit.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
+import {Observable, Subject, merge} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map} from 'rxjs/operators';
+import {ToastrService} from 'ngx-toastr';
+
+import {CompraService} from '../compra.service';
+
+import {CompraDetail} from '../compra-detail';
 
 @Component({
   selector: 'app-compra-edit',
@@ -7,7 +16,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompraEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private compraService: CompraService,
+    
+        private toastrService: ToastrService,
+        private router: Router,
+        private route: ActivatedRoute) { }
+        
+  model: any;
+    /**
+    * La compra que será actualizada
+    */
+    compra: CompraDetail;
+
+    @Input() compra_id: number;
+    
+    
+    
+   
+    
+    
+    getCompra(): void {
+        this.compraService.getCompraDetail(this.compra_id).subscribe(compra => {
+            
+            this.compra = compra;
+        });
+    }
+    
+   
+    
+    checkBoxCompra (): boolean {
+        var element = <HTMLInputElement> document.getElementById("compraEnvio");
+        var isChecked = element.checked;
+        return isChecked;
+    }
+    cancelEdition(): void {
+        this.toastrService.warning('La compra no fue editada', 'Compra edition');
+        this.router.navigate(['/compras/list']);
+    }
+    
+    
+    
+    updateCompra(): void {
+        
+        this.compraService.updateCompra(this.compra)
+            .subscribe(() => {
+                this.router.navigate(['/compras/' + this.compra.id]);
+                this.toastrService.success("La compra fue editada", 'Compra edition');
+            });
+    }
 
   ngOnInit() {
   }
