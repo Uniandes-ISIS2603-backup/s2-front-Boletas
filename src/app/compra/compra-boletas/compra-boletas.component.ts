34b41969c045c8a450220e9dfed7bdcc402816da
srodriguezb1 <s.rodriguezb1@uniandes.edu.co>
2018-11-28 15:28:19 -0500

    Componente

---
+++ src/app/compra/compra-boletas/compra-boletas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compra-boletas',
+  templateUrl: './compra-boletas.component.html',
+  styleUrls: ['./compra-boletas.component.css']
+})
+export class CompraBoletasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
